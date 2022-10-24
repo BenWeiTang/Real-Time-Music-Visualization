f610be04d84b5e672579279b80cb328907acb061
Ben Tang <sjps30503@gmail.com>
2022-10-24 13:38:21 -0400

    Implemented PitchIdentification
    Really it should be name note identification tho....

--- Real Time Music Visualization/Note.cpp
+++ Real Time Music Visualization/Note.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include "Note.hpp"
 
 Note::Note(const double& freq, const sf::Vector2f& startPosition, const sf::Vector2f& velocity)
@@ -18,4 +19,39 @@ void Note::Update()
 const sf::CircleShape& Note::GetCircleShape() const
 {
 	return m_CircleShape;
+}
+
+const Pitch::Pitch Note::IdentityPitch(double freq)
+{
+	int keyNum = std::round(12*std::log2(freq/440.0)); // A will be 0, A# will be 1, ... 
+	keyNum %= 12;
+	switch (keyNum)
+	{
+	case 0:
+		return Pitch::Pitch::A;
+	case 1:
+		return Pitch::Pitch::b;
+	case 2:
+		return Pitch::Pitch::B;
+	case 3:
+		return Pitch::Pitch::C;
+	case 4:
+		return Pitch::Pitch::d;
+	case 5:
+		return Pitch::Pitch::D;
+	case 6:
+		return Pitch::Pitch::e;
+	case 7:
+		return Pitch::Pitch::E;
+	case 8:
+		return Pitch::Pitch::F;
+	case 9:
+		return Pitch::Pitch::g;
+	case 10:
+		return Pitch::Pitch::G;
+	case 11:
+		return Pitch::Pitch::a;
+	default:
+		break;
+	}
 }
--- Real Time Music Visualization/Note.hpp
+++ Real Time Music Visualization/Note.hpp
@@ -20,4 +20,6 @@ private:
 	sf::Vector2f m_Position;
 	sf::Vector2f m_velocity;
 	sf::CircleShape m_CircleShape; //TODO: Remove later
+
+	static const Pitch::Pitch IdentityPitch(double freq);
 };
