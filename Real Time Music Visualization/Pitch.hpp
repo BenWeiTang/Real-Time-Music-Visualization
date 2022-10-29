1cf375a01d2320504440c933723ea9f09e203e7f
Ben Tang <sjps30503@gmail.com>
2022-10-28 21:24:08 -0400

    Update Pitch.hpp

--- Real Time Music Visualization/Pitch.hpp
+++ Real Time Music Visualization/Pitch.hpp
@@ -20,8 +20,43 @@ namespace Pitch
 
 	static inline short Interval(const Pitch& a, const Pitch& b)
 	{
-		short absInterval = std::abs((short)a - (short)b);
-		absInterval = std::min(absInterval, (short)(360 - absInterval));
-		return absInterval;
+		short aValue = (short)a;
+		short bValue = (short)b;
+		short distance = std::abs(aValue - bValue);
+		distance = std::min(distance, (short)(360 - distance));
+		return distance * (aValue > bValue) - distance * (bValue > aValue);
+	}
+
+	static inline sf::Color GetIntervalColor(const short& interval)
+	{
+		switch (interval)
+		{
+		case 0:
+			return sf::Color::Color(255, 255, 255, 64); // White
+		case 30:
+			return sf::Color::Color(0, 255, 128, 64); // Spring Green
+		case 60:
+			return sf::Color::Color(0, 255, 255, 64); // Cyan
+		case 90:
+			return sf::Color::Color(0, 128, 255, 64); // Azure
+		case 120:
+			return sf::Color::Color(0, 0, 255, 64); // Blue
+		case 150:
+			return sf::Color::Color(128, 0, 255, 64); // Violet
+		case 180 || -180:
+			return sf::Color::Color(255, 0, 255, 64); // Magenta
+		case -30:
+			return sf::Color::Color(128, 255, 0, 64); // Chartreuse
+		case -60:
+			return sf::Color::Color(255, 255, 0, 64); // Yellow
+		case -90:
+			return sf::Color::Color(255, 128, 0, 64); // Orange
+		case -120:
+			return sf::Color::Color(255, 0, 0, 64); // Red
+		case -150:
+			return sf::Color::Color(255, 0, 128, 64); // Rose
+		default:
+			break;
+		}
 	}
 }
