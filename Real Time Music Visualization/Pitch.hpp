ea8fc5b09abb54fabee2e9b25afe8927e7d07d69
Ben Tang <sjps30503@gmail.com>
2022-11-04 22:06:18 -0400

    New color generation for intervals

--- Real Time Music Visualization/Pitch.hpp
+++ Real Time Music Visualization/Pitch.hpp
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <stdlib.h> // srand, rand
+#include <time.h>
+#include "ColorModel.hpp"
+
 namespace Pitch
 {
-	enum Pitch : short 
+	enum Pitch : short
 	{
 		C = 0,   // C
 		d = 210, // C sharp or D flat
@@ -29,32 +33,40 @@ namespace Pitch
 
 	static inline sf::Color GetIntervalColor(const short& interval)
 	{
+		std::srand(time(NULL));
+		const static double hue = std::rand() % 360;
+		const static double saturation = 0.125; // Saturation for perfect unisons
+		const static double value = 1.0; // Value for perfect unisons
+		const static double alpha = 0.125; // Alpha for all
+		const static double hueStep = 90.0 / 6.0; // When interval is 180 deg apart (6 steps), rotate around the color wheel 90 deg
+		const static double satStep = 1.0 / 12.0; // When interval is 180 deg apart (6 steps), saturation should increase by 50%, thus divided by 12 (6 x 2)
+		const static double valueStep = value / 12.0; // When interval is 180 deg apart (6 steps), value should be halved, thus divided by 12 (6 x 2)
 		switch (interval)
 		{
 		case 0:
-			return sf::Color::Color(255, 255, 255, 32); // White
+			return HSV_to_RGB(hue, saturation, value, alpha); // Perfect unison
 		case 30:
-			return sf::Color::Color(0, 255, 128, 32); // Spring Green
+			return HSV_to_RGB(std::fmod((hue + 1 * hueStep), 360), saturation + 1 * satStep, value - 1 * valueStep, alpha); // Up 1 perfect fifth
 		case 60:
-			return sf::Color::Color(0, 255, 255, 32); // Cyan
+			return HSV_to_RGB(std::fmod((hue + 2 * hueStep), 360), saturation + 2 * satStep, value - 2 * valueStep, alpha); // Up 2 perfect fifth
 		case 90:
-			return sf::Color::Color(0, 128, 255, 32); // Azure
+			return HSV_to_RGB(std::fmod((hue + 3 * hueStep), 360), saturation + 3 * satStep, value - 3 * valueStep, alpha); // Up 3 perfect fifth
 		case 120:
-			return sf::Color::Color(0, 0, 255, 32); // Blue
+			return HSV_to_RGB(std::fmod((hue + 4 * hueStep), 360), saturation + 4 * satStep, value - 4 * valueStep, alpha); // Up 4 perfect fifth
 		case 150:
-			return sf::Color::Color(128, 0, 255, 32); // Violet
+			return HSV_to_RGB(std::fmod((hue + 5 * hueStep), 360), saturation + 5 * satStep, value - 5 * valueStep, alpha); // Up 5 perfect fifth
 		case 180 || -180:
-			return sf::Color::Color(255, 0, 255, 32); // Magenta
+			return HSV_to_RGB(std::fmod((hue + 6 * hueStep), 360), saturation + 6 * satStep, value - 6 * valueStep, alpha); // Up 6 perfect fifth
 		case -30:
-			return sf::Color::Color(128, 255, 0, 32); // Chartreuse
+			return HSV_to_RGB(std::fmod((hue - 1 * hueStep), 360), saturation + 1 * satStep, value - 1 * valueStep, alpha); // Down 1 perfect fifth
 		case -60:
-			return sf::Color::Color(255, 255, 0, 32); // Yellow
+			return HSV_to_RGB(std::fmod((hue - 2 * hueStep), 360), saturation + 2 * satStep, value - 2 * valueStep, alpha); // Down 2 perfect fifth
 		case -90:
-			return sf::Color::Color(255, 128, 0, 32); // Orange
+			return HSV_to_RGB(std::fmod((hue - 3 * hueStep), 360), saturation + 3 * satStep, value - 3 * valueStep, alpha); // Down 3 perfect fifth
 		case -120:
-			return sf::Color::Color(255, 0, 0, 32); // Red
+			return HSV_to_RGB(std::fmod((hue - 4 * hueStep), 360), saturation + 4 * satStep, value - 4 * valueStep, alpha); // Down 4 perfect fifth
 		case -150:
-			return sf::Color::Color(255, 0, 128, 32); // Rose
+			return HSV_to_RGB(std::fmod((hue - 5 * hueStep), 360), saturation + 5 * satStep, value - 5 * valueStep, alpha); // Down 5 perfect fifth
 		default:
 			break;
 		}
